138f0d04704e554eebc4a3a87be3e132bbb33786
Hanna Lakus <annalakus@ukr.net>
2024-10-19 00:39:17 +0100

    initial files

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Your contacts' });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
