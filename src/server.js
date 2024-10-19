baec434b3d7b79f6ba8142451f9b0433ad9e90b2
Hanna Lakus <annalakus@ukr.net>
2024-10-20 00:25:20 +0100

    add status

--- src/server.js
+++ src/server.js
@@ -23,9 +23,11 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
-    res
-      .status(200)
-      .json({ message: 'Successfully found contacts!', data: contacts });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
@@ -33,11 +35,12 @@ export const setupServer = () => {
     const contact = await getContactById(contactId);
 
     if (!contact) {
-      res.status(404).json({ message: 'Contact not found' });
+      res.status(404).json({ status: 404, message: 'Contact not found' });
       return;
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
@@ -45,6 +48,7 @@ export const setupServer = () => {
 
   app.use((err, req, res, next) => {
     res.status(500).json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
